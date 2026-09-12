ed7e407ddea864d507e0987c55e441fcab5c54c6
FLAZE1 <miruonlymir@gmail.com>
2026-09-12 19:40:15 +0200

    smth new

---
+++ 12.09.26.py
@@ -0,0 +1,101 @@
+from random import*
+from math import*
+# a=randrange(0,50)
+# print(a)
+
+# numbers=[1,2,3,4,5]
+# shuffle(numbers)
+# print(numbers)
+
+# a = randint(1,6)
+# print(a)
+
+# a = random()
+# a *=1000
+# a = a**1/2
+# print(floor(a))
+
+# x=25
+# a=bin(x)
+# b=int("25",8)
+# c=int("25",16)
+# a=format(25,"x")
+# print(a,b,c)
+
+# a=12
+# b=15
+# hex(a)
+# hex(b)
+# print(a+b)
+
+# a = uniform(-10,10)
+# print(round(a,1))
+
+# nums = [10,20,30,40,50,60]
+# a = sample(nums,2)
+# print(a[0]+a[1])
+
+# a=randint(1,20)
+# b=randint(1,20)
+# print(a)
+# print(b)
+# print(a>b)
+
+# a=randrange(0,30,2)
+# print(a)
+
+
+# a = ceil(uniform(10,20))
+# print(a)
+
+# animals = ['кот','собака','хомяк','попугай']
+# print(choice(animals))
+
+# a=int(input())
+# a=a**2
+# print(format(a,"o"))
+
+
+# x=(40-10)//3
+# print(format(x,"x"))
+
+# a=10
+# b=15
+# c=17
+# f = a * (c-b)
+# l = f ** 2
+# k = c % 3 +f
+# n = k//l
+# print(n)
+
+# a = int(input(),16)
+# b = int("1010",2)
+# c = a + b
+# print(bin(a)[2:],c)
+
+# x=(-2416+768)//2*-1
+# print(format(x,"b"))
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+#
+# num = a**2
+# num2 = b*c
+# num3 = num+num2
+# print(format(num3,"b"))
+
+a=int(input(),8)
+b=int(input(),8)
+f=format(a,"d")
+l=format(b,"d")
+a=a**3
+b=b//3
+num = a+b
+print(format(num,"x"))
+
+
+
+
+
+
