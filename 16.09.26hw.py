42fec7155ba80e2c8a13aea8c98cef4e1d9d6cca
Flaze <miruonlymir@gmail.com>
2026-09-16 15:55:48 +0200

    smth new

---
+++ 16.09.26hw.py
@@ -0,0 +1,34 @@
+#1
+# a=7
+# b=14
+# c=21
+# shg1=a*(c-b)
+# shg2=b**2
+# shg3=(shg1+c%5)//shg2
+# print(bin(shg3)[2:])
+
+#2
+# a = input()
+#
+# a = int(a,16)
+# a = a * 5 + a % 8
+# a = a // 3
+# print(oct(a)[2:])
+
+#3
+# a=9
+# b=12
+# c=20
+# shg1 = a ** b
+# shg2 = c // 4
+# shg3 = shg1 + shg2
+# print(hex(shg3)[2:])
+
+#4
+# from math import*
+# a=input()
+# b=input()
+# a=int(a,8)
+# b=int(b,8)
+# f=sqrt(a) + b / 3
+# print(floor(f))
